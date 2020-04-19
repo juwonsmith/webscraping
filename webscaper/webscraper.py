bde5903e78a35051df1ec3e1d9dcd511ea814c13
oladele juwon <59305152+juwonsmith@users.noreply.github.com>
2020-04-19 22:29:36 +0100

    Add files via upload

---
+++ webscaper/webscraper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+try:
+    class get_corona:
+            def __init__(self,country):
+                self.country = country
+                base_url = "https://www.worldometers.info/coronavirus/country/"
+                loc = self.country+"/"
+                url = base_url +  loc
+                corona = requests.get(url)
+                corona_soup= BeautifulSoup(corona.text, "html.parser")
+                for i in corona_soup.findAll("div", {"style": "margin-top:15px"}):
+                    print(i.text)
+                for i in corona_soup.findAll("div", {"style" : "color:#8ACA2B "}):
+                    print("recovered:")
+                    print(i.text) 
+    def call():      
+        conc = input("please enter the country name: ")
+        if  conc == "" :
+            x = input("you didnt input a country,do you want to restart the app,(yes/no): ")
+            if x =="yes":
+             call()
+            else:
+                print('thank you for your time')
+        else:
+            some_country = get_corona(conc)
+    call()
+except :
+    print("there has been an error ")
